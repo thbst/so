bf9d021b15708bf5d6b404ff6114609f59fdbbbe
thbst <thbst84@gmail.com>
2015-03-15 00:13:29 -0700

    [tema] added debug support

--- teme/tema/tema.c
+++ teme/tema/tema.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+#define DEBUG 1
+
+#ifdef DEBUG
+#define dprintf(msg,...) printf("[%s]:%d " msg,__FILE__,__LINE__,##__VA_ARGS__)
+#else
+#define dprintf(msg,...)
+#endif
+
 int main(void)
 {
+	int var=1;
 	printf("SO ... hello world!\n");
-
+	dprintf("debug message var=%d\n",var);
 	return 0;
 }
