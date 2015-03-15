c511c78a6a93a488533266a5760db8f7a01a84d0
thbst <thbst84@gmail.com>
2015-03-15 00:42:28 -0700

    [tema] added util.c / util.h

--- teme/tema/util.c
+++ teme/tema/util.c
@@ -2,19 +2,14 @@
 #include "util.h"
 
 #define DEBUG 1
-
 #ifdef DEBUG
 #define dprintf(msg,...) printf("[%s]:%d " msg,__FILE__,__LINE__,##__VA_ARGS__)
 #else
 #define dprintf(msg,...)
 #endif
 
-
-int main(void)
+void functie()
 {
-	int var=1;
-	functie();
-	printf("SO ... hello world!\n");
-	dprintf("debug message var=%d\n",var);
-	return 0;
+	printf("Current line %d in file %s \n", __LINE__, __FILE__ );
+	dprintf("test message from util.c\n");
 }
